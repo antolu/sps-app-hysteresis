157fbc652e0f91a35c6fd4ab88c386a4d9d8d305
Anton Lu <anton96lu@gmail.com>
2023-04-17 17:38:42 +0200

    fully functioning acquisition

--- sps_apps/hysteresis_prediction/async_utils/_async.py
+++ sps_apps/hysteresis_prediction/async_utils/_async.py
@@ -42,11 +42,11 @@ class Signal:
     @staticmethod
     def _get_loop() -> asyncio.AbstractEventLoop:
         try:
-            loop = asyncio.get_running_loop()
+            loop = asyncio.get_event_loop()
         except RuntimeError:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            return loop
+        return loop
 
     def emit(self, *args: Any) -> None:
         """
