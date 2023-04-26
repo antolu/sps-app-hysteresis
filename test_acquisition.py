a85a5aa179e178579fb8277bdf79682a52c53887
Anton Lu <anton96lu@gmail.com>
2023-04-26 17:19:16 +0200

    save measured data

--- test_acquisition.py
+++ test_acquisition.py
@@ -6,13 +6,13 @@ from argparse import ArgumentParser
 from pathlib import Path
 from signal import SIGINT, SIGTERM, signal
 
-
 from sps_apps.hysteresis_prediction.data import Acquisition, SingleCycleData
 
 log = logging.getLogger()
 
 
 OUTPUT_PATH = Path(__file__).parent / "output"
+save = False
 
 
 def buffer_handler(buffer: list[SingleCycleData], save: bool = False) -> None:
@@ -54,12 +54,22 @@ def main() -> None:
     parser.add_argument(
         "-s", "--save", action="store_true", help="Save buffers to file."
     )
-    parser.parse_args()
+    parser.add_argument(
+        "-m",
+        "--measured",
+        action="store_true",
+        help="Buffer only measured data.",
+    )
+    args = parser.parse_args()
+    global save
+    save = args.save
 
     setup_logging()
     OUTPUT_PATH.mkdir(exist_ok=True)
 
-    acq = Acquisition(min_buffer_size=150000)
+    acq = Acquisition(
+        min_buffer_size=150000, buffer_only_measured=args.measured
+    )
     acq.new_buffer_data.connect(lambda x: buffer_handler(x, save=True))
     acq.new_measured_data.connect(new_measured_handler)
 
