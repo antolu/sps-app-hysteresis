54aa78471e8d809355f046ef89ada68eadbb6515
Anton Lu <anton.lu@cern.ch>
2024-02-21 22:23:04 +0100

    remove flake8

--- sps_apps/hysteresis_prediction/__init__.py
+++ sps_apps/hysteresis_prediction/__init__.py
@@ -1,7 +1,1 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version(__name__)
-
-except PackageNotFoundError:
-    __version__ = "unknown"
+from ._version import version as __version__  # noqa
