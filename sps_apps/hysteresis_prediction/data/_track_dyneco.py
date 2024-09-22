38b6cb24239b636fc701cd408cd4e6e0afd5b29d
Anton Lu <anton.lu@cern.ch>
2024-09-22 17:21:41 +0200

    add converters to track dyneco and fulleco

---
+++ sps_apps/hysteresis_prediction/data/_track_dyneco.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import logging
+
+import hystcomp_utils.cycle_data
+import numpy as np
+import pyda
+import pyda.data
+import pyda_japc
+import datetime
+from qtpy import QtCore
+
+from ._event_builder_abc import (
+    EventBuilderAbc,
+    Subscription,
+)
+
+log = logging.getLogger(__name__)
+
+
+PARAM_DYNECO_IREF = "rda3://UCAP-NODE-SPS-HYSTCOMP-TEST/SPS.MBI.DYNECO/IREF"
+
+
+class AddProgrammedEventBuilder(EventBuilderAbc):
+    def __init__(
+        self,
+        provider: pyda_japc.JapcProvider | None = None,
+        *,
+        parent: QtCore.QObject | None = None,
+    ):
+        super().__init__(
+            subscriptions=[
+                Subscription("DYNECO", PARAM_DYNECO_IREF),
+            ],
+            provider=provider,
+            parent=parent,
+        )
+
+        self._cycle_data_buffer: dict[str, hystcomp_utils.cycle_data.CycleData] = {}
+
+    def _handle_acquisition_impl(
+        self, fspv: pyda.data.PropertyRetrievalResponse
+    ) -> None:
+        parameter = str(fspv.query.endpoint)
+        selector = str(fspv.query.context)
+        header = fspv.value.header
+        cycle_time_s = str(header.cycle_time())[:-7]
+        id_ = f"[{selector}@{cycle_time_s}]"
+
+        if parameter != PARAM_DYNECO_IREF:
+            msg = f"Received unknown acquisition for {parameter}@{selector}."
+            raise ValueError(msg)
+
+        if selector not in self._cycle_data_buffer:
+            log.error(f"Received trigger for cycle without data: {selector}.")
+            return
+
+        cycle_data = self._cycle_data_buffer[selector]
+
+        i_prog_df = fspv.value.get("value")
+        iref = np.vstack((i_prog_df.xs, i_prog_df.ys))
+
+        if not np.allclose(cycle_data.cycle_timestamp, header.cycle_timestamp):
+            msg = (
+                f"{cycle_data} Cycle timestamp mismatch: {cycle_data.cycle_timestamp} "
+                f"!= {header.cycle_timestamp}. This probably means that the cycle data "
+                f"comes from a different cycle instance than the IREF data."
+            )
+            log.error(msg)
+            raise ValueError(msg)
+
+        now = datetime.datetime.now()
+        msg = (
+            f"{id_} Triggered by new DYNECO signal from IREF at {now}."
+            f"Changing cycle data to DYNECO cycle and updating programmed current."
+        )
+        log.debug(msg)
+
+        if cycle_data.cycle.endswith("ECO"):
+            msg = f"{id_} Cycle data is already ECO. This shouldn't happen."
+            log.error(msg)
+            return None
+
+        orig_cycle = cycle_data.cycle
+        cycle_data.cycle += "_DYNECO"
+        cycle_data.current_prog = iref
+
+        msg = f"{id_} Updated cycle data {orig_cycle} -> {cycle_data.cycle}."
+        log.debug(msg)
+
+        self.onNewCycleData(cycle_data)
+
+    def onNewCycleData(self, cycle_data: hystcomp_utils.cycle_data.CycleData) -> None:
+        msg = f"[{cycle_data}]: Received data, saving to buffer."
+        log.debug(msg)
+
+        self._cycle_data_buffer[cycle_data.user] = cycle_data
---
+++ sps_apps/hysteresis_prediction/data/_track_fulleco.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+import logging
+
+import hystcomp_utils.cycle_data
+import numpy as np
+import pyda
+import pyda.data
+import pyda_japc
+from qtpy import QtCore
+
+from ._event_builder_abc import (
+    BufferedSubscription,
+    BufferedSubscriptionEventBuilder,
+    Subscription,
+)
+
+log = logging.getLogger(__name__)
+
+
+PARAM_FULLECO_IREF = "rda3://UCAP-NODE-SPS-HYSTCOMP-TEST/SPS.MBI.FULLECO/IREF"
+TRIGGER = "XTIM.SX.FCY-MMODE-CT/Acquisition"
+
+
+class AddProgrammedEventBuilder(BufferedSubscriptionEventBuilder):
+    def __init__(
+        self,
+        provider: pyda_japc.JapcProvider | None = None,
+        *,
+        parent: QtCore.QObject | None = None,
+    ):
+        super().__init__(
+            subscriptions=[
+                Subscription("MMODE", TRIGGER),
+            ],
+            buffered_subscriptions=[
+                BufferedSubscription("FULLECO_IREF", PARAM_FULLECO_IREF),
+            ],
+            provider=provider,
+            parent=parent,
+        )
+
+        self._cycle_data_buffer: dict[str, hystcomp_utils.cycle_data.CycleData] = {}
+
+    def _handle_acquisition_impl(
+        self, fspv: pyda.data.PropertyRetrievalResponse
+    ) -> None:
+        parameter = str(fspv.query.endpoint)
+        selector = str(fspv.query.context)
+
+        if parameter == TRIGGER:
+            if selector not in self._cycle_data_buffer:
+                log.error(f"Received trigger for cycle without data: {selector}.")
+                return
+
+            cycle_data = self._cycle_data_buffer[selector]
+
+            mmode = fspv.value.get("MACHInE_MODE")
+
+            if mmode != "FULLECO":
+                log.info(f"{cycle_data} is not in FULLECO mode: {mmode}")
+                return None
+
+            log.info(f"{cycle_data} is in FULLECO mode")
+
+            if cycle_data.cycle.endswith("FULLECO"):
+                log.info(f"{cycle_data} is already in FULLECO mode")
+                return None
+            if cycle_data.cycle.endswith("DYNECO"):
+                log.info(f"{cycle_data} is in DYNECO mode")
+                return None
+
+            cycle_data.cycle = f"{cycle_data.cycle}_FULLECO"
+
+            msg = f"[{cycle_data}]: Adding FULLECO programmed current."
+            log.debug(msg)
+
+            i_prog_df = self._get_buffered_data(PARAM_FULLECO_IREF, selector).value.get(
+                "value"
+            )
+            i_prog = np.vstack((i_prog_df.xs, i_prog_df.ys))
+
+            if i_prog[1, 0] != i_prog[1, -1]:
+                msg = (
+                    "The programmed current does not start and end at the same value: "
+                )
+                msg += f"start={i_prog[1, 0]}, end={i_prog[1, -1]}"
+                log.warning(msg)
+
+            cycle_data.current_prog = i_prog
+
+            msg = f"[{cycle_data}]: Added FULLECO programmed current."
+            log.debug(msg)
+
+            if cycle_data.cycle.endswith("ECO"):
+                msg = f"[{cycle_data}]: ECO cycle has already had programs updated."
+                log.debug(msg)
+                self.cycleDataAvailable.emit(cycle_data)
+                return
+
+            self.onNewCycleData(cycle_data)
+
+        # unknown parameter
+        msg = f"Received unknown acquisition for {parameter}@{selector}."
+        raise ValueError(msg)
+
+    def onNewCycleData(self, cycle_data: hystcomp_utils.cycle_data.CycleData) -> None:
+        msg = f"[{cycle_data}]: Received data, saving to buffer."
+        log.debug(msg)
+
+        self._cycle_data_buffer[cycle_data.user] = cycle_data
