54ed86c04899141ab91d595c0aaa05688957294d
Anton Lu <anton.lu@cern.ch>
2024-09-25 00:39:04 +0200

    fix start cycle emit

--- sps_apps/hysteresis_prediction/data/_start_cycle.py
+++ sps_apps/hysteresis_prediction/data/_start_cycle.py
@@ -53,7 +53,7 @@ class StartCycleEventBuilder(EventBuilderAbc):
         msg = f"[{cycle_data}]: Cycle is starting."
         log.debug(msg)
 
-        self.cycleDataAvailable(cycle_data)
+        self.cycleDataAvailable.emit(cycle_data)
 
     def onNewCycleData(self, cycle_data: hystcomp_utils.cycle_data.CycleData) -> None:
         msg = f"[{cycle_data}]: Received data, saving to buffer."
