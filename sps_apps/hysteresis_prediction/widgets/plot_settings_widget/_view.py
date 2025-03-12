536c31da55071bfbb049058eb939c936f535c076
Anton Lu <anton.lu@cern.ch>
2025-03-12 13:47:15 +0100

    remove references to status

--- sps_apps/hysteresis_prediction/widgets/plot_settings_widget/_view.py
+++ sps_apps/hysteresis_prediction/widgets/plot_settings_widget/_view.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import logging
-import math
 from typing import Any
 
 import hystcomp_utils.cycle_data
@@ -10,7 +9,6 @@ from qtpy.QtWidgets import QWidget
 
 from ...generated.plot_settings_widget_ui import Ui_PlotSettingsWidget
 from ...utils import run_in_main_thread
-from ._status import LOG_MESSAGES, AppStatus
 
 log = logging.getLogger(__package__)
 
@@ -22,7 +20,6 @@ class PlotSettingsWidget(Ui_PlotSettingsWidget, QWidget):
     timespan_changed = Signal(int, int)  # min, max
     downsample_changed = Signal(int)
     toggle_predictions = Signal(bool)
-    status_changed = Signal(AppStatus)
 
     def __init__(self, parent: QWidget | None = None):
         QWidget.__init__(self, parent=parent)
@@ -34,25 +31,9 @@ class PlotSettingsWidget(Ui_PlotSettingsWidget, QWidget):
         self.spinBoxTimespan.valueChanged.connect(self._timespan_changed)
         self.spinBoxDownsample.valueChanged.connect(self.downsample_changed)
         self.buttonResetAxis.clicked.connect(self._timespan_changed)
-        # self.buttonPredict.clicked.connect(self._on_prediction_toggle)
-
-        self.status_changed.connect(self._on_new_status)
-
-        # self.buttonPredict.setEnabled(False)
-        self.buttonPredict.hide()
-
-    @run_in_main_thread
-    def on_model_loaded(self) -> None:
-        """Called when the model is loaded. Enable prediction button."""
-        # self.buttonPredict.setEnabled(True)
 
     def _timespan_changed(self, *_: Any) -> None:
         self.timespan_changed.emit(self.spinBoxTimespan.value(), 0)
-
-    @run_in_main_thread
-    def _on_new_status(self, status: AppStatus) -> None:
-        message = LOG_MESSAGES[status]
-        self.labelStatus.setText(message)
 
     @Slot(hystcomp_utils.cycle_data.CycleData)
     @run_in_main_thread
@@ -68,28 +49,3 @@ class PlotSettingsWidget(Ui_PlotSettingsWidget, QWidget):
     def blink_led(self) -> None:
         self.Led.setStatus(self.Led.Status.ON)
         QTimer.singleShot(500, lambda: self.Led.setStatus(self.Led.Status.OFF))
-
-    @run_in_main_thread
-    def _on_prediction_toggle(self, *_: Any) -> None:
-        if self._prediction_enabled:
-            self._prediction_enabled = False
-            self.buttonPredict.setText("Start Predictions")
-        else:
-            self._prediction_enabled = True
-            self.buttonPredict.setText("Stop Predictions")
-
-        self.toggle_predictions.emit(self._prediction_enabled)
-
-    @run_in_main_thread
-    def _set_progressbar(self, value: int, total: int) -> None:
-        try:
-            new_value = value / total * 100 if value < total else 100
-            log.debug(f"Setting progress bar value to {new_value}.")
-            self.progressBar.setValue(math.ceil(new_value))
-
-            if new_value >= 100:
-                self.progressBar.hide()
-            else:
-                self.progressBar.show()
-        except:  # noqa E722
-            log.exception("An exception occurred while setting progress bar value.")
