107e1cbef24bb96c7c777ceefe070897856e13f5
Anton Lu <anton.lu@cern.ch>
2025-07-04 18:34:21 +0200

    change timing devices to WRT

--- sps_app_hysteresis/contexts/_params.py
+++ sps_app_hysteresis/contexts/_params.py
@@ -3,15 +3,15 @@ from __future__ import annotations
 from ._base_context import EddyCurrentModel, ParameterNames, RemoteParameterNames
 
 MBI_PARAMS = ParameterNames(
-    TRIGGER="SX.CZERO-CTML/CycleWarning",
+    TRIGGER="XTIM.SX.FCY2500-WRT/Acquisition",
     I_PROG="rmi://virtual_sps/MBI/IREF",
     B_PROG="rmi://virtual_sps/SPSBEAM/B",
     ADD_PROG_TRIGGER="rda3://UCAP-NODE-SPS-HYSTCOMP/UCAP.SX.INJ-100/Acquisition",
     B_CORRECTION="rda3://UCAP-NODE-SPS-HYSTCOMP/SPSBEAM.BHYS-CORRECTION/Acquisition",
-    CYCLE_START="XTIM.SX.SCY-CT/Acquisition",
+    CYCLE_START="XTIM.SX.SCY-WRT/Acquisition",
     I_PROG_DYNECO="rda3://UCAP-NODE-SPS-HYSTCOMP/SPS.MBI.DYNECO/IREF",
     I_PROG_FULLECO="rda3://UCAP-NODE-SPS-HYSTCOMP/SPS.MBI.FULLECO/IREF",
-    FULLECO_TRIGGER="XTIM.SX.FCY-MMODE-CT/Acquisition",
+    FULLECO_TRIGGER="XTIM.SX.FCY-MMODE-WRT/Acquisition",
     I_MEAS="MBI/LOG.I.MEAS",
     B_MEAS="SR.BMEAS-SP-B-SD/CycleSamples#samples",
     RESET_REFERENCE_TRIGGER="rmi://virtual_sps/SPSBEAM/B",
